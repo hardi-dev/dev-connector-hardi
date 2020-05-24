60ea8f7105c9f49f294167a726ac693248afb7ec
Hardiansa <hardiansa@maleo.social>
2020-05-25 06:28:44 +0700

    :fire: add Request and Body Validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator')
 
-router.get('/', (req, res) => res.send('User Route'));
+router.post('/', [
 
-module.exports = router;
+  check('name', 'Name is Required').not().isEmpty(), 
+  check('email', 'Please inculde a valid email').isEmail(),
+  check('password', 'Please enter a password with 6 or more character').isLength({ min: 6})
+
+], (req, res) => {
+
+  const errors = validationResult(req); 
+  if( !errors.isEmpty() ) {
+    return res.status(400).json({
+      errors: errors.array()
+    })
+  }
+  
+  console.log(req.body);
+  res.send('User Route')
+
+});
+
+module.exports = router; 
