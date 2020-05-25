781298b4c11c56fcfdbd063a3a43cc6956f9db12
Hardiansa <hardiansa@maleo.social>
2020-05-25 22:06:41 +0700

    :fire: Implement Remove User and Profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -172,6 +172,29 @@ router.get('/user/:user_id', async (req, res) => {
 })
 
 
+// @route       DELETE api/profile/
+// @desc        Delete Profile, User and Post
+// @access      Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    
+    // @todo Remove Users Posts
+
+    // Remove Profile 
+    await Profile.findOneAndRemove({ user: req.user.id });
+
+    // Remove User
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: 'User deleted!' });
+
+  } catch (error) { 
+    console.error(error);
+    res.status(500).send("Server Error")
+  }
+})
+
+
 
 
 module.exports = router;
