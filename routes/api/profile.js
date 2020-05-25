0fdc0fec58f308c08f68ff20fee970055301bb01
Hardiansa <hardiansa@maleo.social>
2020-05-25 08:11:15 +0700

    :fire: Get current loged in user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
-router.get('/', (req, res) => res.send('Profile Route'));
+// Get Profile
+// Private
+router.get('/me', auth, async (req, res) => {
+  try {
+    
+    const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatr']);
+
+    // Check Profile Is Not Exists
+    if( !profile ) {
+      return res.status(400).json({ errors: [{ msg: 'There are no Profile for this user '}] });
+    }
+
+
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error")
+  }
+});
 
 module.exports = router;
