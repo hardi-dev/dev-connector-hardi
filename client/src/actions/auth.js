befc4b2e2c57daae3367f4fe52c7c180387232c6
Hardiansa <hardiansa@maleo.social>
2020-05-26 20:51:08 +0700

    :fire: Implement User Login

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -4,12 +4,13 @@ import {
   REGISTER_FAIL,
   USER_LOADED,
   AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
 } from './../actions/types';
 import { setAlert } from './alert';
 import setAuthToken from './../utils/setAuthToken';
 
 // Load User
-
 export const loadUser = () => async (dispatch) => {
   if (localStorage.token) {
     setAuthToken(localStorage.token);
@@ -41,6 +42,8 @@ export const register = ({ name, email, password }) => async (dispatch) => {
     const response = await axios.post('/api/users/', body, config);
 
     dispatch({ type: REGISTER_SUCCESS, payload: response.data });
+
+    dispatch(loadUser());
   } catch (err) {
     const errors = err.response.data.errors;
 
@@ -49,5 +52,32 @@ export const register = ({ name, email, password }) => async (dispatch) => {
     }
 
     dispatch({ type: REGISTER_FAIL });
+  }
+};
+
+// Login User
+export const login = (email, password) => async (dispatch) => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  const body = JSON.stringify({ email, password });
+
+  try {
+    const response = await axios.post('/api/auth/', body, config);
+
+    dispatch({ type: LOGIN_SUCCESS, payload: response.data });
+
+    dispatch(loadUser());
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({ type: LOGIN_FAIL });
   }
 };
