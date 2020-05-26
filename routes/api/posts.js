48ff46f0ff6a69d2808f66c2046c31751b1ad49e
Hardiansa <hardiansa@maleo.social>
2020-05-26 10:47:57 +0700

    :fire: Add Post Route and make user able to add new Post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,50 @@
 const express = require('express');
 const router = express.Router();
+const {
+  check,
+  validationResult
+} = require('express-validator');
+const auth = require('../../middleware/auth');
 
-router.get('/', (req, res) => res.send('Posts Route'));
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+
+// @route       POST api/post
+// @desc        Create Post
+// @access      private
+router.post('/', [auth, [
+  check('text', 'Text is required').not().isEmpty()
+]], async (req, res) => {
+
+  const errors = validationResult(req);
+
+  if( !errors.isEmpty() ) {
+    return res.status(500).json({ errors: errors.array() });
+  }
+
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+
+    const newPost = new Post({
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id
+    })
+
+    const posts = await newPost.save();
+
+    res.send(posts);
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server Error");
+  }
+
+
+
+});
 
 module.exports = router;
