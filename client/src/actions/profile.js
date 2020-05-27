46efb24d2a4157bffc6a4ff56cb282b692f732df
Hardiansa <hardiansa@maleo.social>
2020-05-27 09:12:21 +0700

    :fire: Create Profile Action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -22,3 +22,50 @@ export const getCurrentUserProfile = () => async (dispatch) => {
     });
   }
 };
+
+// Create or Update a Profile
+export const createProfile = (formData, history, edit = false) => async (
+  dispatch
+) => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const response = await axios.post('/api/profile', formData, config);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: response.data,
+    });
+
+    dispatch(
+      setAlert(
+        edit ? 'Profile updated successfully' : 'Profile Created successfully',
+        'success'
+      )
+    );
+
+    if (!edit) {
+      history.push('/dashboard');
+    }
+
+    // const body = JSON.stringify(formData);
+  } catch (error) {
+    const errors = error.response.data.errors;
+
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      },
+    });
+  }
+};
