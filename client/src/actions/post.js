eeda292b5684c578aa3e9f2c5f4690e1f9e5dac9
hardi-dev <ardititanium@gmail.com>
2020-05-31 04:47:20 +0700

    :fire: Display Single Post

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -2,19 +2,40 @@ import axios from 'axios';
 import { setAlert } from './alert.js';
 import {
   GET_POSTS,
+  GET_POST,
   POST_ERROR,
   UPDATE_LIKES,
   DELETE_POST,
   ADD_POST,
 } from './types';
 
-// Get Post
+// Get Posts
 export const getPosts = () => async (dispatch) => {
   try {
     const response = await axios.get('/api/posts');
 
     dispatch({
       type: GET_POSTS,
+      payload: response.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      },
+    });
+  }
+};
+
+// Get Post
+export const getPost = (postId) => async (dispatch) => {
+  try {
+    const response = await axios.get(`/api/posts/${postId}`);
+
+    dispatch({
+      type: GET_POST,
       payload: response.data,
     });
   } catch (error) {
--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -11,6 +11,7 @@ const PostItem = ({
   deletePost,
   auth,
   post: { _id, text, name, avatar, user, date, likes, comments },
+  showActions,
 }) => {
   return (
     <div className='post bg-white p-1 my-1'>
@@ -25,46 +26,55 @@ const PostItem = ({
         <p className='post-date'>
           Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
         </p>
-        <button
-          type='button'
-          onClick={(e) => addLike(_id)}
-          className='btn btn-light'
-        >
-          <i className='fas fa-thumbs-up'></i>
-          <span>
-            {likes.length > 0 && (
+        {showActions && (
+          <Fragment>
+            <button
+              type='button'
+              onClick={(e) => addLike(_id)}
+              className='btn btn-light'
+            >
+              <i className='fas fa-thumbs-up'></i>
               <span>
-                {'  '}
-                {likes.length}{' '}
+                {likes.length > 0 && (
+                  <span>
+                    {'  '}
+                    {likes.length}{' '}
+                  </span>
+                )}
               </span>
+            </button>
+            <button
+              type='button'
+              onClick={(e) => removeLike(_id)}
+              className='btn btn-light'
+            >
+              <i className='fas fa-thumbs-down'></i>
+            </button>
+            <Link to={`/posts/${_id}`} className='btn btn-primary'>
+              Discussion{' '}
+              {comments.length > 0 && (
+                <span className='comment-count'>{comments.length}</span>
+              )}
+            </Link>
+
+            {!auth.loading && user === auth.user._id && (
+              <button
+                type='button'
+                onClick={(e) => deletePost(_id)}
+                className='btn btn-danger'
+              >
+                <i className='fas fa-times'></i>
+              </button>
             )}
-          </span>
-        </button>
-        <button
-          type='button'
-          onClick={(e) => removeLike(_id)}
-          className='btn btn-light'
-        >
-          <i className='fas fa-thumbs-down'></i>
-        </button>
-        <Link to={`/post/${_id}`} className='btn btn-primary'>
-          Discussion{' '}
-          {comments.length > 0 && (
-            <span className='comment-count'>{comments.length}</span>
-          )}
-        </Link>
-        {!auth.loading && user === auth.user._id && (
-          <button
-            type='button'
-            onClick={(e) => deletePost(_id)}
-            className='btn btn-danger'
-          >
-            <i className='fas fa-times'></i>
-          </button>
+          </Fragment>
         )}
       </div>
     </div>
   );
+};
+
+PostItem.defaultProps = {
+  showActions: true,
 };
 
 PostItem.propTypes = {
